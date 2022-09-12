765dabbca6aa70f22483762d979c793610a5ffbf
Shurayukii <47608467+Shurayukii@users.noreply.github.com>
2022-09-12 19:41:00 +0200

    Update manual updater but it's automatic.bat

--- manual updater but it's automatic.bat
+++ manual updater but it's automatic.bat
@@ -1,2 +1,2 @@
 @echo off
-cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git
+cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git --branch Shurayukii
