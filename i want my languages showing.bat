6af1d95a2e6c83094ad962d3a6f9763bab49b882
Shurayukii <47608467+Shurayukii@users.noreply.github.com>
2022-09-16 16:40:59 +0200

    too little bats

--- i want my languages showing.bat
+++ i want my languages showing.bat
@@ -1,2 +1,8 @@
 @echo off
 cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git
+@echo off
+cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git
+@echo off
+cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git
+@echo off
+cd .. && rd /s /q "Simplicity" && git clone https://github.com/Shurayukii/Simplicity.git
